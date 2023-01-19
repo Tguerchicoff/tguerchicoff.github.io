4463bd9e0612c14386f677eb40f52692530da40b
Tomás Guerchicoff Adamo <87401454+Tguerchicoff@users.noreply.github.com>
2023-01-19 18:14:35 -0300

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,16 @@
+//redirecciones
+function redirectStudies(){
+    location.href= "studies.html";          
+}
+
+function redirectExp(){
+    location.href= "experience.html";          
+}
+
+function redirectHab(){
+    location.href= "habilities.html";          
+}
+
+function redirectHome(){
+    location.href= "index.html";    
+}
